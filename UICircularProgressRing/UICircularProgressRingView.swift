2b830695b5c0b4e8f02025f186c24dbe781b9114
Luis Padron <luispadronedu@gmail.com>
2016-09-14 22:31:02 -0400

    Add code for inner ring angles
    - Add code to correctly calculate rotation of inner ring
    - Add property observers to all public properties

--- UICircularProgressRing/UICircularProgressRingView.swift
+++ UICircularProgressRing/UICircularProgressRingView.swift
@@ -21,18 +21,39 @@ public class UICircularProgressRingView: UIView {
     /// The value of the current progress. Range [0, maxValue]
     @IBInspectable public var value: CGFloat = 0 {
         didSet {
-            self.drawValueLabel()
+            self.setNeedsDisplay()
         }
     }
     /// The value of the maximum amount of progress. Range [0, ∞]
-    @IBInspectable public var maxValue: CGFloat = 100
+    @IBInspectable public var maxValue: CGFloat = 100 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
     
     // MARK: Outer Ring properties
     
-    @IBInspectable public var outerRingWidth: CGFloat = 10.0
-    @IBInspectable public var outerRingColor: UIColor = UIColor.gray
-    @IBInspectable public var outerStartAngle: CGFloat = 0
-    @IBInspectable public var outerEndAngle: CGFloat = 360
+    @IBInspectable public var outerRingWidth: CGFloat = 10.0 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    @IBInspectable public var outerRingColor: UIColor = UIColor.gray {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    @IBInspectable public var startAngle: CGFloat = 0 {
+        didSet {
+            // Make sure view is redrawn when this property is set
+            self.setNeedsDisplay()
+        }
+    }
+    @IBInspectable public var enndAngle: CGFloat = 360 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
     @IBInspectable public var outerRingStyle: Int = 1 {
         didSet {
             switch self.outerRingStyle{
@@ -41,17 +62,29 @@ public class UICircularProgressRingView: UIView {
             case 3: self.outStyle = CGLineCap.square
             default: self.outStyle = CGLineCap.butt
             }
+            
+            self.setNeedsDisplay()
         }
     }
     private var outStyle: CGLineCap = .butt
     
     // MARK: Inner Ring properties
     
-    @IBInspectable public var innerRingWidth: CGFloat = 5.0
-    @IBInspectable public var innerRingColor: UIColor = UIColor.blue
-    @IBInspectable public var innerRingSpacing: CGFloat = 1
-    @IBInspectable public var innerStartAngle: CGFloat = 0
-    @IBInspectable public var innerEndAngle: CGFloat = 360
+    @IBInspectable public var innerRingWidth: CGFloat = 5.0 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    @IBInspectable public var innerRingColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    @IBInspectable public var innerRingSpacing: CGFloat = 1 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
     @IBInspectable public var innerRingStyle: Int = 2 {
         didSet {
             switch self.innerRingStyle {
@@ -60,32 +93,46 @@ public class UICircularProgressRingView: UIView {
             case 3: self.inStyle = CGLineCap.square
             default: self.inStyle = CGLineCap.butt
             }
+            
+            self.setNeedsDisplay()
         }
     }
     private var inStyle: CGLineCap = .round
     
     // MARK: Booleans
-    
     @IBInspectable var rotateClockWise: Bool = true
     
     // MARK: Label
     /// The label for the value, will change an animate when value is set
     lazy private var valueLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-    @IBInspectable public var textColor: UIColor = UIColor.black
-    @IBInspectable public var fontSize: CGFloat = 18
-    @IBInspectable public var valueIndicator: String = "%"
+    
+    @IBInspectable public var textColor: UIColor = UIColor.black {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    @IBInspectable public var fontSize: CGFloat = 18 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    @IBInspectable public var valueIndicator: String = "%" {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
     @IBInspectable public var showFloatingPoint: Bool = false
     
     
     // MARK: Methods
     
     override public func draw(_ rect: CGRect) {
-        drawRings()
+        drawOuterRing()
+        drawInnerRing()
         drawValueLabel()
     }
     
-    /// Draws the inner and outer rings
-    private func drawRings() {
+    private func drawOuterRing() {
         // Properties of view
         let center = CGPoint(x: bounds.midX, y: bounds.midY)
         
@@ -93,27 +140,36 @@ public class UICircularProgressRingView: UIView {
         let radiusOut = max(bounds.width, bounds.height)
         let outerPath = UIBezierPath(arcCenter: center,
                                      radius: radiusOut/2 - outerRingWidth/2,
-                                     startAngle: outerStartAngle.toRads,
-                                     endAngle: outerEndAngle.toRads,
+                                     startAngle: startAngle.toRads,
+                                     endAngle: enndAngle.toRads,
                                      clockwise: rotateClockWise)
         
         outerPath.lineWidth = outerRingWidth
         outerRingColor.setStroke()
         outerPath.lineCapStyle = outStyle
         outerPath.stroke()
-        outerPath.close()
+    }
+    
+    private func drawInnerRing() {
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        let angleDiff: CGFloat = enndAngle.toRads - startAngle.toRads
+        let arcLenPerValue = angleDiff / CGFloat(maxValue)
+        
+        let innerEndAngle = arcLenPerValue * CGFloat(value) + startAngle.toRads
         
         // Draw the inner path
         let radiusIn = max(bounds.width - outerRingWidth*2 - innerRingSpacing,
                            bounds.height - outerRingWidth*2 - innerRingSpacing)
         let innerPath = UIBezierPath(arcCenter: center,
-                                     radius: radiusIn/2 - innerRingWidth/2, startAngle: innerStartAngle.toRads, endAngle: innerEndAngle.toRads, clockwise: true)
+                                     radius: radiusIn/2 - innerRingWidth/2,
+                                     startAngle: startAngle.toRads,
+                                     endAngle: innerEndAngle,
+                                     clockwise: true)
         
         innerPath.lineWidth = innerRingWidth
         innerRingColor.setStroke()
         innerPath.lineCapStyle = inStyle
         innerPath.stroke()
-        innerPath.close()
     }
     
     /// Draws the value label inside of the progress rings
@@ -128,9 +184,11 @@ public class UICircularProgressRingView: UIView {
         self.addSubview(valueLabel)
     }
     
-    public func setValue(_ newVal: CGFloat, animate: Bool) {
+    public func setValue(_ newVal: CGFloat, animated: Bool) {
+        if animated {
+            
+        }
+        
         self.value = newVal
     }
-    
-
 }
