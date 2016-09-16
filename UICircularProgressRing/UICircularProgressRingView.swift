6d44c4a6ea98cf86c550723bfcc48a2c21032f4f
Luis Padron <luispadronedu@gmail.com>
2016-09-16 18:36:02 -0400

    more code documentation, and minor clean up

--- UICircularProgressRing/UICircularProgressRingView.swift
+++ UICircularProgressRing/UICircularProgressRingView.swift
@@ -56,7 +56,7 @@ private extension UILabel {
  
  Circular progress ring view.
  
- - Author:
+ ## Author:
  Luis Padron
  
  */
@@ -66,11 +66,11 @@ private extension UILabel {
     /**
      The delegate for the progress ring
      
-     - Important:
+     ## Important ##
      In order to delegate class must conform to UICircularProgressRingDelegate
      and override progressRingAnimtionDidFinish(_:)
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     public var delegate: UICircularProgressRingDelegate?
@@ -80,12 +80,12 @@ private extension UILabel {
     /**
      The value property for the progress ring. ex: (23)/100
      
-     - Important:
+     ## Important ##
      Default = 0
      When assigning to this var the view will be redrawn.
      Recommended to assign value using setValue(_:) instead.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var value: CGFloat = 0 {
@@ -110,11 +110,12 @@ private extension UILabel {
      The max value for the progress ring. ex: 23/(100)
      Used to calculate amount of progress depending on self.value and self.maxValue
      
-     - Important:
+     ## Important ##
      Default = 100
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var maxValue: CGFloat = 100 {
@@ -125,18 +126,22 @@ private extension UILabel {
     
     /**
      Variable for the style of the progress ring.
+     
      Range: [1,4]
+     
      The four styles are
+     
      - 1: Radius of the inner ring is smaller (inner ring inside outer ring)
      - 2: Radius of inner ring is equal to outer ring (both at same location)
      - 3: Radius of inner ring is equal to outer ring, and the outer ring is dashed
      - 4: Radius of inner ring is equal to outer ring, and the outer ring is dotted
      
-     - Important:
+     ## Important ##
      Default = 1
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var progressRingStyle: Int = 1 {
@@ -148,11 +153,12 @@ private extension UILabel {
     /**
      An array of CGFloats, used to calculate the dash length for progressRingStyle = 3
      
-     - Important:
+     ## Important ##
      Default = [7.0, 7.0]
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     public var patternForDashes: [CGFloat] = [7.0, 7.0] {
@@ -167,11 +173,12 @@ private extension UILabel {
     /**
      The width of the outer ring for the progres bar
      
-     - Important:
+     ## Important ##
      Default = 10.0
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var outerRingWidth: CGFloat = 10.0 {
@@ -182,11 +189,12 @@ private extension UILabel {
     /**
      The color for the outer ring
      
-     - Important:
+     ## Important ##
      Default = UIColor.gray
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var outerRingColor: UIColor = UIColor.gray {
@@ -200,12 +208,14 @@ private extension UILabel {
      Please note that Cocoa Touch uses a clockwise rotating unit circle.
      I.e: 90 degrees is at the bottom and 270 degrees is at the top
      
-     - Important:
+     ## Important ##
      Default = 0 (degrees)
+     
      Values should be in degrees
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var startAngle: CGFloat = 0 {
@@ -219,12 +229,14 @@ private extension UILabel {
      Please note that Cocoa Touch uses a clockwise rotating unit circle.
      I.e: 90 degrees is at the bottom and 270 degrees is at the top
      
-     - Important:
+     ## Important ##
      Default = 0 (degrees)
+     
      Values should be in degrees
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var endAngle: CGFloat = 360 {
@@ -240,11 +252,12 @@ private extension UILabel {
      - 3: Line with a square end
      - <1 & >3: Defaults to style 1
      
-     - Important:
+     ## Important ##
      Default = 1
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var outerRingCapStyle: Int = 1 {
@@ -263,7 +276,7 @@ private extension UILabel {
      The private variable for outerRingCapStyle
      When the user sets the style, using a property observer this is then set
      
-     - Important:
+     ## Important ##
      Default = CGLineCap.butt
      */
     private var outStyle: CGLineCap = .butt
@@ -273,11 +286,12 @@ private extension UILabel {
     /**
      The width of the inner ring for the progres bar
      
-     - Important:
+     ## Important ##
      Default = 5.0
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var innerRingWidth: CGFloat = 5.0 {
@@ -288,11 +302,12 @@ private extension UILabel {
     /**
      The color of the inner ring for the progres bar
      
-     - Important:
+     ## Important ##
      Default = UIColor.blue
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var innerRingColor: UIColor = UIColor.blue {
@@ -303,12 +318,14 @@ private extension UILabel {
     /**
      The spacing between the outer ring and inner ring
      
-     - Important:
+     ## Important ##
      This only applies when using progressRingStyle = 1
+     
      Default = 1
+     
      When assigning to this var the view will be redrawn.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var innerRingSpacing: CGFloat = 1 {
@@ -318,17 +335,20 @@ private extension UILabel {
     }
     /**
      The style for the inner ring line (how it is drawn on screen)
+     
      Range [1,3]
+     
      - 1: Line with a squared off end
      - 2: Line with a rounded off end
      - 3: Line with a square end
      - <1 & >3: Defaults to style 2
      
-     - Important:
+     ## Important ##
      Default = 2
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var innerRingCapStyle: Int = 2 {
@@ -347,7 +367,7 @@ private extension UILabel {
      The private variable for innerRingCapStyle
      When the user sets the style; using a property observer this is then set
      
-     - Important:
+     ## Important ##
      Default = kCALineCapRound
      */
     private var inCapStyle: String = kCALineCapRound
@@ -365,11 +385,12 @@ private extension UILabel {
      If false the current value will not be shown.
      The value label is also never added as a subview or drawn
      
-     - Important:
+     ## Important ##
      Default = true
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var shouldShowValueText: Bool = true {
@@ -380,11 +401,12 @@ private extension UILabel {
     /**
      The text color for the value label field
      
-     - Important:
+     ## Important ##
      Default = UIColor.black
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var textColor: UIColor = UIColor.black {
@@ -395,11 +417,12 @@ private extension UILabel {
     /**
      The text/font size for the value label
      
-     - Important:
+     ## Important ##
      Default = 18
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var fontSize: CGFloat = 18 {
@@ -412,11 +435,12 @@ private extension UILabel {
      Provide name as a string, and make sure "Fonts Provided by application"
      is set inside the Info.plist of the project.
      
-     - Important:
+     ## Important ##
      Default = nil
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var customFontWithName: String? {
@@ -429,11 +453,12 @@ private extension UILabel {
      appened to the value
      Example: "%" -> "100%"
      
-     - Important:
+     ## Important ##
      Default = "%"
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var valueIndicator: String = "%" {
@@ -445,13 +470,14 @@ private extension UILabel {
      A toggle for showing or hiding floating points from
      the value in the value label
      
-     - Important:
+     ## Important ##
      Default = false (dont show)
-     To customize number of decmial places to show
-     assign a value to decimalPlaces.
+     
+     To customize number of decmial places to show, assign a value to decimalPlaces.
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var showFloatingPoint: Bool = false {
@@ -462,12 +488,14 @@ private extension UILabel {
     /**
      The amount of decimal places to show in the value label
      
-     - Important:
+     ## Important ##
      Default = 2
+     
      Only used when showFloatingPoint = true
+     
      When assigning to this var the view will be redrawn
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     @IBInspectable public var decimalPlaces: Int = 2 {
@@ -481,51 +509,54 @@ private extension UILabel {
     /**
      The duration of the animation
      
-     - Important:
+     ## Important ##
      Default = 1.0
+     
      Only used when calling .setValue(animated: true)
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     public var animationDuration: CFTimeInterval = 1.0
     /**
      The type of function animation to use
      
-     - Important:
+     ## Important ##
      Default = kCAMediaTimingFunctionEaseIn
+     
      String should be from kCAMediaTimingFunction_____
+     
      Only used when calling .setValue(animated: true)
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     public var animationStyle: String = kCAMediaTimingFunctionEaseIn
     /**
      Private CAShapeLayer for drawing and animating the progress ring
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     private lazy var shapeLayer = CAShapeLayer()
     /**
      Private Timer for animating purposes
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     private lazy var timer = Timer()
     /**
      Start time for animating value label
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     private lazy var startTime = CACurrentMediaTime()
     /**
      CADisplayLink for animating the label
      
-     -Author:
+     ## Author:
      Luis Padron
      */
     private lazy var link = CADisplayLink()
@@ -536,7 +567,7 @@ private extension UILabel {
      Overriden draw method which draws the outer ring, inner ring,
      and the value label.
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     override public func draw(_ rect: CGRect) {
@@ -550,14 +581,13 @@ private extension UILabel {
     /**
      Sets the current value for the progress ring
      
-     - Parameters:
-     - newVal: The value to be set for the progress ring
-     - animated: Boolean value if the progress ring should be animated or not
+     - Parameter newVal: The value to be set for the progress ring
+     - Parameter animated: Boolean value if the progress ring should be animated or not
      
-     - Important:
+     ## Important ##
      The view will be drawn and animated if set to animate
      
-     - Author:
+     ## Author:
      Luis Padron
      */
     public func setValue(_ newVal: CGFloat, animated: Bool) {
@@ -586,35 +616,45 @@ private extension UILabel {
         outerRingColor.setStroke()
         outerPath.lineCapStyle = outStyle
         
+        // If the style is 3 or 4, make sure to draw either dashes or dotted path
         if progressRingStyle == 3 {
             outerPath.setLineDash(patternForDashes, count: 1, phase: 0.0)
         } else if progressRingStyle == 4 {
             outerPath.setLineDash([0, outerPath.lineWidth * 2], count: 2, phase: 0)
             outerPath.lineCapStyle = .round
         }
-        
+        // Stroke and done
         outerPath.stroke()
     }
     
     private func drawInnerRing() {
-        let center = CGPoint(x: bounds.midX, y: bounds.midY)
-        let angleDiff: CGFloat = endAngle.toRads - startAngle.toRads
-        let arcLenPerValue = angleDiff / CGFloat(maxValue)
+        // Draw the inner path of the ring
         
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        // Calculate the center difference between the end and start angle
+        let angleDiff: CGFloat = endAngle.toRads - startAngle.toRads
+        // Calculate how much we should draw depending on the value set
+        let arcLenPerValue = angleDiff / CGFloat(maxValue)
+        // The inner end angle some basic math is done
         let innerEndAngle = arcLenPerValue * CGFloat(value) + startAngle.toRads
         
+        // The radius for style 1 is set below
+        // The radius for style 1 is a bit less than the outer, this way it looks like its inside the circle
         var radiusIn = (max(bounds.width - outerRingWidth*2 - innerRingSpacing, bounds.height - outerRingWidth*2 - innerRingSpacing)/2) - innerRingWidth/2
         
+        // If the style is different, mae the radius equal to the outerRadius
         if progressRingStyle >= 2 {
             radiusIn = (max(bounds.width, bounds.height)/2) - (outerRingWidth/2)
         }
-        
+        // Start drawing
         let innerPath = UIBezierPath(arcCenter: center,
                                      radius: radiusIn,
                                      startAngle: startAngle.toRads,
                                      endAngle: innerEndAngle,
                                      clockwise: true)
         
+        // Create a shape layer from the path in order to animate later on
         shapeLayer.path = innerPath.cgPath
         shapeLayer.strokeColor = innerRingColor.cgColor
         shapeLayer.fillColor = UIColor.clear.cgColor
@@ -622,15 +662,15 @@ private extension UILabel {
         shapeLayer.strokeEnd = 1.0
         shapeLayer.lineCap = inCapStyle
         shapeLayer.lineJoin = inCapStyle
-        
+        // Add to our sublayer
         self.layer.addSublayer(shapeLayer)
     }
     
-    /// Draws the value label inside of the progress rings
     private func drawValueLabel() {
+        // Draws the text field
         valueLabel.update(withValue: value, centerInView: self, valueIndicator: valueIndicator,
                           showsDecimal: showFloatingPoint, decimalPlaces: decimalPlaces)
-        
+        // Some basic label properties are set
         valueLabel.font = UIFont.systemFont(ofSize: fontSize)
         valueLabel.textAlignment = .center
         valueLabel.textColor = textColor
@@ -639,13 +679,16 @@ private extension UILabel {
             valueLabel.font = UIFont(name: fName, size: fontSize)
         }
         
+        // Make sure to size the label to fit
         valueLabel.sizeToFit()
+        // Deterime what should be the center for the label
         valueLabel.center = CGPoint(x: bounds.midX, y: bounds.midY)
-        
+        // Add it as a subview
         self.addSubview(valueLabel)
     }
     
     private func animateInnerRing() {
+        // Create an animation for the ShapeLayer we defined in drawInnerRing(_:)
         let animation = CABasicAnimation(keyPath: "strokeEnd")
         animation.duration = animationDuration
         animation.fromValue = 0.0
@@ -655,6 +698,7 @@ private extension UILabel {
         animation.fillMode = kCAFillModeForwards
         shapeLayer.add(animation, forKey: "animateInnerRing")
         
+        // If were showing the value text, then animate that as well
         if shouldShowValueText {
             valueLabel.text = "\(oldValue)"
             link = CADisplayLink(target: self, selector: #selector(self.animateLabel))
@@ -666,17 +710,21 @@ private extension UILabel {
     @objc private func animateLabel() {
         let dt = (link.timestamp - startTime) / animationDuration
         
+        // If our animating value is the value the user set then were done animating
         if (dt >= 1.0) {
             valueLabel.update(withValue: value, centerInView: self, valueIndicator: valueIndicator,
                               showsDecimal: showFloatingPoint, decimalPlaces: decimalPlaces)
-            
+            // Remove this animation run loop
             link.remove(from: RunLoop.current, forMode: .commonModes)
+            // Done animating everything, call the delegate method and return
             delegate?.progressRingAnimationDidFinish()
             return
         }
+        // Not done animating, calculate the current value based on time and duration left
         
         let current = (value - oldValue) * CGFloat(dt) + oldValue
         
+        // Update the label with the new current value we came up with
         valueLabel.update(withValue: current, centerInView: self, valueIndicator: valueIndicator,
                           showsDecimal: showFloatingPoint, decimalPlaces: decimalPlaces)
     }
