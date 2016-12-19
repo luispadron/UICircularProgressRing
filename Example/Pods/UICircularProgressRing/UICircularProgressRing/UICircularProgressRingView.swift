4ce000b5d3bf9c1de9cfbd5148da2fc80a40ac35
Luis Padron <Luis@luispadron.com>
2016-12-19 16:33:00 -0500

    Update example project to 1.1.7

--- Example/Pods/UICircularProgressRing/UICircularProgressRing/UICircularProgressRingView.swift
+++ Example/Pods/UICircularProgressRing/UICircularProgressRing/UICircularProgressRingView.swift
@@ -573,6 +573,10 @@ import UIKit
         self.ringLayer.customFontWithName = customFontWithName
         self.ringLayer.showFloatingPoint = showFloatingPoint
         self.ringLayer.decimalPlaces = decimalPlaces
+        
+        // Sets background color to clear, this fixes a bug when placing view in tableview cells
+        self.backgroundColor = UIColor.clear
+        self.ringLayer.backgroundColor = UIColor.clear.cgColor
     }
     
     /**
