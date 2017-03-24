e36568cf7210be3847a9a60f6d37bd1da3e93ccb
Luis Padron <luispadronn@gmail.com>
2017-03-24 18:43:51 -0400

    Move around some scripts
    - Add new run-tests-hook script which we execute and run tests before commiting
    since I keep forgetting to do this

--- run-jazzy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-jazzy --clean -a Luis Padron -u https://luispadron.com -m UICircularProgressRing --module-version 1.1.0 -g https://github.com/luispadron/UICircularProgressRing --theme fullwidth
