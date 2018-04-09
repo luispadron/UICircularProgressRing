3c9b66a8fadd61d0bcc09632b1a6bb85dafd4bc4
Luis Padron <heyluispadron@gmail.com>
2018-04-09 12:24:46 -0400

    Move source files to src directory
    This should fix issues with carthage not building the correct file.
    Updated Jazzy script to work with new directory change

--- scripts/run-jazzy.sh
+++ scripts/run-jazzy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-jazzy --clean --output docs -a Luis Padron -u https://luispadron.com -m UICircularProgressRing --module-version 1.4.2 -g https://github.com/luispadron/UICircularProgressRing --theme apple
+jazzy --clean --source-directory src  --output docs -a Luis Padron -u https://luispadron.com -m UICircularProgressRing --module-version 1.8.5 -g https://github.com/luispadron/UICircularProgressRing --theme apple
+
