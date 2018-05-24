0ab527628443f9633b368f959d33d65bdb18c5e1
Luis Padron <heyluispadron@gmail.com>
2018-05-24 00:20:27 -0400

    Init commit

--- scripts/run-jazzy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-jazzy --clean --source-directory src  --output docs -a Luis Padron -u https://luispadron.com -m UICircularProgressRing --module-version 2.0.0 -g https://github.com/luispadron/UICircularProgressRing --theme apple
-
