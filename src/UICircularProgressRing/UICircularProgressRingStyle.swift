fa7ec7e659a38a35de09726c7b0506c0927c2347
Abdulla Allaith <abdulla.allaith@viitech.net>
2018-07-30 22:51:48 +0300

    Added new ring style: bordered.

--- src/UICircularProgressRing/UICircularProgressRingStyle.swift
+++ src/UICircularProgressRing/UICircularProgressRingStyle.swift
@@ -44,4 +44,6 @@
     case dotted = 4
     /// Inner ring is placed ontop of the outer ring and it has a gradient
     case gradient = 5
+    /// Inner ring is placed ontop of the outer ring and outer ring has border
+    case bordered = 6
 }
