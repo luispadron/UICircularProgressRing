69e805dbec2c5f2ac4cc21109d157462e901c37f
Luis <heyluispadron@gmail.com>
2020-01-06 18:15:05 -0500

    update snapshot tests and images

--- Tests/UICircularProgressRingSnapshotTests.swift
+++ Tests/UICircularProgressRingSnapshotTests.swift
@@ -16,6 +16,7 @@ final class UICircularProgressRingSnapshotTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+        record = true
         ring = UICircularProgressRing()
         ring.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
         ring.startProgress(to: 75, duration: 0)
@@ -28,67 +29,83 @@ final class UICircularProgressRingSnapshotTests: XCTestCase {
     }
 
     func test_defaults() {
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_ontop_style() {
         ring.style = .ontop
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_dashed_style() {
         ring.style = .dashed(pattern: [0.3, 0.5])
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_dotted_style() {
         ring.style = .dotted
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_bordered_style() {
         ring.style = .bordered(width: 2, color: .red)
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_ontop_style_AND_knob() {
         ring.valueKnobStyle = .init(size: 16, color: .orange)
         ring.style = .ontop
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_dashed_style_AND_knob() {
         ring.valueKnobStyle = .init(size: 16, color: .orange)
         ring.style = .dashed(pattern: [0.3, 0.5])
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_dotted_style_AND_knob() {
         ring.valueKnobStyle = .init(size: 16, color: .orange)
         ring.style = .dotted
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_bordered_style_AND_knob() {
         ring.valueKnobStyle = .init(size: 16, color: .orange)
         ring.style = .bordered(width: 2, color: .red)
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_custom_knob_path() {
         ring.valueKnobStyle = .init(size: 16, color: .orange, path: .init({ rect in
             UIBezierPath(roundedRect: rect, cornerRadius: 4)
         }))
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_gradient_default() {
         ring.gradientOptions = .default
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
     }
 
     func test_gradient() {
         ring.gradientOptions = UICircularRingGradientOptions(startPosition: .top, endPosition: .bottom, colors: [.red, .green, .blue], colorLocations: [0, 0.5, 1])
-        assertSnapshot(matching: self.ring, as: .image)
+        verifyRing()
+    }
+
+    func verifyRing(functionName: String = #function) {
+        let controller = UIViewController()
+        controller.loadView()
+        controller.view.addSubview(ring)
+        ring.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            ring.centerYAnchor.constraint(equalTo: controller.view.centerYAnchor),
+            ring.centerXAnchor.constraint(equalTo: controller.view.centerXAnchor),
+            ring.widthAnchor.constraint(equalToConstant: 200),
+            ring.heightAnchor.constraint(equalToConstant: 200),
+        ])
+        controller.view.layoutIfNeeded()
+
+        assertSnapshot(matching: controller, as: .image(on: .iPhone8), testName: functionName)
     }
 }
